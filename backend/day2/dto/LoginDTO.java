0c88b9a699d218fa8ebfd55a37589956f47426c9
BlackDeViL1906 <94516956+BlackDeViL1906@users.noreply.github.com>
2024-02-29 16:08:42 +0530

    Add files via upload

---
+++ backend/day2/dto/LoginDTO.java
@@ -0,0 +1,16 @@
+// package com.recharge.demo.dto;
+
+// import lombok.AllArgsConstructor;
+// import lombok.Builder;
+// import lombok.Data;
+// import lombok.NoArgsConstructor;
+
+// @Data
+// @AllArgsConstructor
+// @NoArgsConstructor
+// @Builder
+// public class LoginDTO {
+//     private Long id;
+//     private String username;
+//     private String email;
+// }
