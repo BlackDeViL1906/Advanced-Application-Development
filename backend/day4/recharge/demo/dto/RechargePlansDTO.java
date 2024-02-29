6778bc54b6715402083708422c3b27d6593e26c0
BlackDeViL1906 <94516956+BlackDeViL1906@users.noreply.github.com>
2024-02-29 16:06:27 +0530

    Add files via upload

---
+++ backend/day4/recharge/demo/dto/RechargePlansDTO.java
@@ -0,0 +1,18 @@
+package com.recharge.demo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class RechargePlansDTO {
+    private String serviceProvider;
+    private String planPrice;
+    private String planValidity;
+    private String planData;
+    private String planSubscriptions;
+}
