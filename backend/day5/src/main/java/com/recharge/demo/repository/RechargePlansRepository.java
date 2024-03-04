92aa351eab2fa1918c74f8e375bed11420536460
BlackDeViL1906 <94516956+BlackDeViL1906@users.noreply.github.com>
2024-03-04 11:26:46 +0530

    Add files via upload

---
+++ backend/day5/src/main/java/com/recharge/demo/repository/RechargePlansRepository.java
@@ -0,0 +1,12 @@
+package com.recharge.demo.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.recharge.demo.model.RechargePlans;
+import java.util.List;
+
+
+@Repository
+public interface RechargePlansRepository extends JpaRepository<RechargePlans, Long> {
+ List<RechargePlans> findByServiceprovider(String serviceprovider);
+}
